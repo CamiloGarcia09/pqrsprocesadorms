66085fbdabd7bc6f0fa1d3888b3d84e28b54c52f
Camilo Garcia <juan092005ca@gmail.com>
2025-02-19 21:32:50 -0500

    Se generó la estructura base del procesador pqrs para el proyecto Ucoshop

---
+++ src/main/java/co/ucoshop/pqrsprocesadorms/PqrsprocesadormsApplication.java
@@ -0,0 +1,13 @@
+package co.ucoshop.pqrsprocesadorms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PqrsprocesadormsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PqrsprocesadormsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/ucoshop/pqrsprocesadorms/PqrsprocesadormsApplicationTests.java
@@ -0,0 +1,13 @@
+package co.ucoshop.pqrsprocesadorms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PqrsprocesadormsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
