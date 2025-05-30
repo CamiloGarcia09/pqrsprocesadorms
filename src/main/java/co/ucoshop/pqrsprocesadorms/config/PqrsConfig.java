b8f455a80d0030898e4a12dfaf0a879732c307cc
marcojr282 <marco.zapata282@gmail.com>
2025-05-29 20:40:58 -0500

    Create PqrsConfig

---
+++ src/main/java/co/ucoshop/pqrsprocesadorms/config/PqrsConfig.java
@@ -0,0 +1,21 @@
+package co.ucoshop.pqrsprocesadorms.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Setter
+@Getter
+@Configuration
+@ConfigurationProperties(prefix = "procesadorpqrs.pqrs")
+public class PqrsConfig {
+
+    private String crearQn;
+    private String crearEx;
+    private String crearRk;
+
+    private String deleteQn;
+    private String deleteEx;
+    private String deleteRk;
+}
